4c4d58c6d97ba9f926668c1d66d0ec1261808aa4
10kaoru12 <10kaoru12@gmail.com>
2018-11-25 22:56:31 +0900

    q1-1.cの更新

--- q1-1.c
+++ q1-1.c
@@ -1,40 +1,12 @@
 #include <stdio.h>
-#include <math.h>
-#include <unistd.h>
-#define N 1000
 
 int main(void)
 {
     FILE *gp;
 
-    int i;
-    int j;
-    double x, y, z, t;
-
     gp = popen("gnuplot -persist", "w");
-    fprintf(gp, "set xrange [-1.2:1.2]\n");
-    fprintf(gp, "set yrange [-1.2:1.2]\n");
-    fprintf(gp, "set zrange [0:10]\n");
-    fprintf(gp, "set xlabel \"x\"\n");
-    fprintf(gp, "set ylabel \"y\"\n");
-    fprintf(gp, "set zlabel \"z\"\n");
-
-    for (j = 0; j < N; ++j)
-    {
-        usleep(1);
-        fprintf(gp, "splot '-' with line\n");
-        for (i = 0; i < N; ++i)
-        {
-            t = 0.05 * i;
-            x = sin(t + 0.1 * j);
-            y = tan(t + 0.1 * j);
-            z = t;
-            fprintf(gp, "%f\t%f\t%f\n", x, y, z);
-        }
-        fprintf(gp, "e\n");
-    }
-
-    fprintf(gp, "exit\n");
+    fprintf(gp, "set ticslevel 0\n");
+    fprintf(gp, "splot x**2 + y**2\n");
     pclose(gp);
 
     return 0;
