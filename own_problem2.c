a5d7e148f63c206beb2a00a57067639363d0030a
10kaoru12 <10kaoru12@gmail.com>
2018-12-13 22:28:45 +0900

    自作問題の作成

---
+++ own_problem2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int allocate(int **array, int size, int value)
+{
+    *array = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
+        *(*array + i) = value;
+    }
+
+    return 0;
+}
+int main(void)
+{
+    int *v = NULL;
+    int size = 5;
+    int value = 100;
+
+    allocate(&v, size, value);
+    for (int i = 0; i < size; i++)
+    {
+        printf("v[%d]=%d\n", i, v[i]);
+    }
+
+    return 0;
+}
