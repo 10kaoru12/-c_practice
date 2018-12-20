30f8482383d1f6324c2b876a3e02d9f1f00eb4e6
10kaoru12 <10kaoru12@gmail.com>
2018-12-20 15:26:54 +0900

    code reformat

--- exercise3.c
+++ exercise3.c
@@ -4,13 +4,13 @@ int *max_element(int *ptr, int n);
 
 int main(void) {
 	const int n = 10;
-	int* return_ptr;
+	int *return_ptr;
 	int str[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	int *ptr;
 	ptr = str;
-	return_ptr=max_element(ptr,n);
-	printf("最大値のアドレスは%p\n",return_ptr);
-	printf("最大値は%d",*return_ptr);
+	return_ptr = max_element(ptr, n);
+	printf("最大値のアドレスは%p\n", return_ptr);
+	printf("最大値は%d", *return_ptr);
 	return 0;
 }
 
